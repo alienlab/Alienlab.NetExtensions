43102ccf5eabdb68ad1db51819ece58e80ff05b2
Alen Pelin <AlenPelin@sitecore.net>
2015-08-19 16:26:16 +1000

    Signing and releasing as 1.0.8.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,13 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Alienlab.NetExtensions")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Alienlab.NetExtensions")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("27f0c57d-0696-40d2-ab0e-187f2e652428")]
-[assembly: AssemblyFileVersion("1.0.7.0")]
-[assembly: AssemblyVersion("1.0.7.0")]
+
+[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
